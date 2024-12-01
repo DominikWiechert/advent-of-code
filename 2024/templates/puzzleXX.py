b7883a7cc3a3b2cd9643f9ef07e2ac04f480ba72
DESKTOP-C2RA6LC\dominik <dominik-wiechert@freenet.de>
2024-12-01 16:02:13 +0100

    Add template and helper functions

---
+++ 2024/templates/puzzleXX.py
@@ -0,0 +1,11 @@
+from helper_functions import get_input
+
+def first_puzzle():
+    input = get_input()
+
+def second_puzzle():
+    input = get_input()
+
+if __name__ == '__main__':
+    first_puzzle()
+    second_puzzle()
