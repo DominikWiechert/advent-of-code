659b455157362e8da48bd7f52ea741566b8cfb9f
DESKTOP-C2RA6LC\dominik <dominik-wiechert@freenet.de>
2024-12-03 10:07:52 +0100

    Solution Day 3

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,7 @@
 def get_input():
     with open('input.txt', 'r') as f:
         return f.read().splitlines()
+
+def read_file_as_string(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
